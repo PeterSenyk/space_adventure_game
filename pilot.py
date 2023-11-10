fa8ab956aead38cb1809b261faf9ae61ab65674e
PeterSenyk <Peter.Senyk@gmail.com>
2023-11-10 14:41:16 -0800

    ADD shield and targeting

--- pilot.py
+++ pilot.py
@@ -7,16 +7,23 @@ def select_ship(player_stats):
     :return:
     """
     print(player_stats["Name"], "Here are your choices for a ship")
-    print("ANVIL ARROW: Attack [1], Movement [3], HP [5] Cargo Space [2]")
-    print("AEGIS GLADIUS: Attack [2], Movement [2], HP [5], Cargo Space [2]")
-    print("DRAKE BUCCANEER: Attack [3], Movement [2], HP [4], Cargo Space [2]")
-    new_ship = input("Please select A for the ANVIL ARROW, G for the AEGIS GLADIUS, or B for the DRAKE BUCCANEER\n")
+    print("ANVIL ARROW: Attack [1], Movement [3], HP [5], Shield [2], Targeting [4], Cargo Space [2]")
+    print("AEGIS GLADIUS: Attack [2], Movement [2], HP [5], Shield [2], Targeting [4], Cargo Space [2]")
+    print("DRAKE BUCCANEER: Attack [3], Movement [2], HP [4], Shield [2], Targeting [4], Cargo Space [2]")
+    new_ship = input("Please select:\n[A] for the ANVIL ARROW\n"
+                     "[G] for the AEGIS GLADIUS\n[B] for the DRAKE BUCCANEER\n")
     if new_ship.upper() == "A":
-        return {"Ship": "ANVIL ARROW", "Attack": 1, "Movement": 3, "HP": 5, "Cargo": 2}
+        return {"Ship": "ANVIL ARROW", "Attack": 1,
+                "Movement": 3, "HP": 5, "Targeting": 4,
+                "Shield": 2, "Cargo": 2}
     if new_ship.upper() == "G":
-        return {"Ship": "AEGIS GLADIUS", "Attack": 2, "Movement": 2, "HP": 5, "Cargo": 2}
+        return {"Ship": "AEGIS GLADIUS", "Attack": 2,
+                "Movement": 2, "HP": 5, "Targeting": 4,
+                "Shield": 2, "Cargo": 2}
     if new_ship.upper() == "B":
-        return {"Ship": "DRAKE BUCCANEER", "Attack": 3, "Movement": 2, "HP": 4, "Cargo": 2}
+        return {"Ship": "DRAKE BUCCANEER", "Attack": 3,
+                "Movement": 2, "HP": 4, "Targeting": 4,
+                "Shield": 2, "Cargo": 2}
 
 
 def make_player():
