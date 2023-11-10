3c997a0dcad0fd64723e4f43858e1c979ed6a795
Peter Senyk <Peter.Senyk@gmail.com>
2023-11-09 17:54:20 -0800

    ADD make_player and select_ship

--- space_sud.py
+++ space_sud.py
@@ -9,28 +9,29 @@ def game():
     """
     runs the game
     """
-    rows = 3
-    columns = 3
-    easy = [0, 0, 5]
+    rows = 5
+    columns = 5
     board = make_board(rows, columns)
-    character = make_character(easy)
+    player_stats = make_player()
+    player_ship = select_ship(player_stats)
+    character = {"Stats": player_stats, "Ship": player_ship}
     achieved_goal = False
     there_is_a_challenger = False
     describe_current_location(rows, columns, board, character)
-    print(f"You're in the bottom-left hand corner of a maze [grid (0,0)], the goal is at the "
-          f"top-right [gird ({rows - 1},{columns - 1})")
-    while is_alive(character) and not achieved_goal:
-        direction = get_user_choice()
-        valid_move = validate_move(board, character, direction)
-        if valid_move:
-            move_character(character, direction)
-            describe_current_location(rows, columns, board, character)
-            there_is_a_challenger = check_for_challenger()
-        if there_is_a_challenger:
-            guessing_game(character)
-        achieved_goal = check_if_goal_attained(rows, columns, character)
-    if character.get("HP") == 0:
-        print("You died")
+    # print(f"You're in the bottom-left hand corner of a maze [grid (0,0)], the goal is at the "
+    #       f"top-right [gird ({rows - 1},{columns - 1})")
+    # while is_alive(character) and not achieved_goal:
+    #     direction = get_user_choice()
+    #     valid_move = validate_move(board, character, direction)
+    #     if valid_move:
+    #         move_character(character, direction)
+    #         describe_current_location(rows, columns, board, character)
+    #         there_is_a_challenger = check_for_challenger()
+    #     if there_is_a_challenger:
+    #         guessing_game(character)
+    #     achieved_goal = check_if_goal_attained(rows, columns, character)
+    # if character.get("HP") == 0:
+    #     print("You died")
 
 
 def check_if_goal_attained(rows, columns, character):
@@ -61,45 +62,45 @@ def is_alive(character):
     :precondition: character HP value must start above zero
     :return: Boolean True of False
     """
-    if character.get("HP") <= 0:
+    if character["Ship"]["HP"] <= 0:
         return False
     else:
         return True
 
 
-def guessing_game(character):
-    """
-    Challenges the character to a guessing game
+# def guessing_game(character):
+#     """
+#     Challenges the character to a guessing game
+#
+#     this function creates a random number between 1-5 inclusive, and asks the character to guess
+#
+#     :param character: a dictionary of character location and HP
+#     :precondition: character must have an HP value of 1 or greater
+#     :post-condition: if the character guesses correctly they move on, if they guess wrong they loose 1 HP
+#     :return: a string based on whether the character guessed right or wrong
+#     """
+#     number_to_guess = r.randint(1, 5)
+#     player_guess = int(input("A challenger approaches... Test your luck and guess a number between 1 & 5 :\n"))
+#     if player_guess == number_to_guess:
+#         print("Lucky guess, this time...  HP=", character.get("HP"))
+#         return character["HP"]
+#     else:
+#         character["HP"] -= 1
+#         print("Wrong! the number was", number_to_guess, "Your HP =", character.get("HP"))
+#         return character["HP"]
 
-    this function creates a random number between 1-5 inclusive, and asks the character to guess
 
-    :param character: a dictionary of character location and HP
-    :precondition: character must have an HP value of 1 or greater
-    :post-condition: if the character guesses correctly they move on, if they guess wrong they loose 1 HP
-    :return: a string based on whether the character guessed right or wrong
-    """
-    number_to_guess = r.randint(1, 5)
-    player_guess = int(input("A challenger approaches... Test your luck and guess a number between 1 & 5 :\n"))
-    if player_guess == number_to_guess:
-        print("Lucky guess, this time...  HP=", character.get("HP"))
-        return character["HP"]
-    else:
-        character["HP"] -= 1
-        print("Wrong! the number was", number_to_guess, "Your HP =", character.get("HP"))
-        return character["HP"]
-
-
-def check_for_challenger():
-    """
-    Checks if there is a challenger at the character location
-
-    this functions checks if there's a challenger by chance by using a random integer between 1-4 inclusive
-
-    post-condition: if the random integer is 1, there will be a challenger at the character location
-    :return: Boolean True
-    """
-    if r.randint(1, 4) == 1:
-        return True
+# def check_for_challenger():
+#     """
+#     Checks if there is a challenger at the character location
+#
+#     this functions checks if there's a challenger by chance by using a random integer between 1-4 inclusive
+#
+#     post-condition: if the random integer is 1, there will be a challenger at the character location
+#     :return: Boolean True
+#     """
+#     if r.randint(1, 4) == 1:
+#         return True
 
 
 def move_calculator(character, direction):
@@ -249,7 +250,28 @@ def make_board(rows, columns):
     return new_board
 
 
-def make_character(starting_stats):
+def select_ship(player_stats):
+    """
+    Selects a ship for the game
+
+    this funtion lets the player select their ship for the game
+
+    :return:
+    """
+    print(player_stats["Name"], "Here are your choices for a ship")
+    print("ANVIL ARROW: Attack [1], Movement [3], HP [5] Cargo Space [2]")
+    print("AEGIS GLADIUS: Attack [2], Movement [2], HP [5], Cargo Space [2]")
+    print("DRAKE BUCCANEER: Attack [3], Movement [2], HP [4], Cargo Space [2]")
+    new_ship = input("Please select A for the ANVIL ARROW, G for the AEGIS GLADIUS, or B for the DRAKE BUCCANEER\n")
+    if new_ship.upper() == "A":
+        return {"Ship": "ANVIL ARROW", "Attack": 1, "Movement": 3, "HP": 5, "Cargo": 2}
+    if new_ship.upper() == "G":
+        return {"Ship": "AEGIS GLADIUS", "Attack": 2, "Movement": 2, "HP": 5, "Cargo": 2}
+    if new_ship.upper() == "B":
+        return {"Ship": "DRAKE BUCCANEER", "Attack": 3, "Movement": 2, "HP": 4, "Cargo": 2}
+
+
+def make_player():
     """
     Makes a character
 
@@ -260,8 +282,11 @@ def make_character(starting_stats):
     :post-condition: a character dictionary is completed with starting coordinates and HP
     :return: a dictionary
     """
-    new_player = {"X-coordinate": starting_stats[0], "Y-coordinate": starting_stats[1], "HP": starting_stats[2]}
-    return new_player
+    player_last_name = input("Please type your last name\n")
+    player_name = "Captain " + player_last_name
+    print("Congratulations", player_name, "on graduating the Space Academy at the top of your class\n")
+    player_stats = {"X-coordinate": 0, "Y-coordinate": 0, "Name": player_name}
+    return player_stats
 
 
 def main():
