8988c679e3724e0a506428cc65c243ab7f7b4d84
Peter Senyk <Peter.Senyk@gmail.com>
2023-11-14 21:26:44 -0800

    ADD unit-test for select_ship

---
+++ unittests/test_select_ship.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from unittest.mock import patch
+from pilot import select_ship
+
+
+class Test(TestCase):
+    @patch('builtins.input', return_value="A")
+    def test_select_anvil_arrow(self, _):
+        player_stats = {"Name": "test_name"}
+        select_ship(player_stats)
+        actual = select_ship(player_stats)
+        expected = {
+            "Ship": "ANVIL ARROW", "Attack": 1, "Movement": 3,
+            "HP": 5, "Targeting": 4, "Shield": 2, "Cargo": 2
+        }
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value="G")
+    def test_select_aegis_gladius(self, _):
+        player_stats = {"Name": "test_name"}
+        select_ship(player_stats)
+        actual = select_ship(player_stats)
+        expected = {
+            "Ship": "AEGIS GLADIUS", "Attack": 2,
+            "Movement": 2, "HP": 5, "Targeting": 4,
+            "Shield": 2, "Cargo": 2
+        }
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value="B")
+    def test_select_drake_buccaneer(self, _):
+        player_stats = {"Name": "test_name"}
+        select_ship(player_stats)
+        actual = select_ship(player_stats)
+        expected = {
+            "Ship": "DRAKE BUCCANEER", "Attack": 3,
+            "Movement": 2, "HP": 4, "Targeting": 4,
+            "Shield": 2, "Cargo": 2
+        }
+        self.assertEqual(expected, actual)
