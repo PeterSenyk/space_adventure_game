caafe984a52803d20c2fcaa672c472bd32fc81e2
PeterSenyk <Peter.Senyk@gmail.com>
2023-11-23 12:11:31 -0800

    REWORK module organization

--- start_game.py
+++ start_game.py
@@ -3,7 +3,33 @@ import pilot
 
 
 def start_of_game():
-    space = boards.make_space(rows, columns)
-    player_stats = pilot.make_player()
-    player_ship = pilot.select_ship(player_stats)
-    character = {"Stats": player_stats, "Ship": player_ship, "Coordinates": {"X-coordinate": 0, "Y-coordinate": 0}}
+    character_created = False
+    character = {"Stats": {"Title": "", "Name": "", "Accolades": []}, "Ship": {}, "Coordinates": {"X-coordinate": 0, "Y-coordinate": 0}}
+    while not character_created:
+        last_name = get_player_last_name()
+
+    pass
+
+
+def get_player_last_name():
+    last_name = capitalize_name(input("Enter your last name to register for the Arc-Corp Space Academy\n"))
+
+
+
+    def capitalize_name(name):
+        last_name = name.strip().lower
+        if len(last_name) == 0:
+            raise ValueError('No empty names allowed!')
+        else:
+            return name.title()
+
+    def makename():
+        capitalized_name = capitalize_name("nicole paige brookes")
+        print(capitalized_name)
+
+        try:
+            another_capitalized_name = capitalize_name("")
+        except ValueError as e:
+            print(e)
+        else:
+            print(another_capitalized_name)
