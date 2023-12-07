622e568b5e956b34a44b65c6b59e30554b09da2f
PeterSenyk <Peter.Senyk@gmail.com>
2023-12-07 15:58:40 -0800

    FIX unit test scan_space_grid

--- unittests/actions/test_scan_space_grid.py
+++ unittests/actions/test_scan_space_grid.py
@@ -7,21 +7,16 @@ from actions import scan_space_grid
 class Test(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_scan_space_grid_2_2(self, mock_stdout):
-        rows = 2
-        columns = 2
         character = {
-            'Stats': {'Name': 'Captain sen'},
-            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 3, 'Movement': 3, 'HP': 5,
-                     'Targeting': 4, 'Shield': 2, 'Cargo': 2},
             'Coordinates': {'X-coordinate': 0, 'Y-coordinate': 0}
         }
         space = {
-            (0, 0): [5, 'You are in the void of space, the sheer amount of nothingness is eerie.'],
-            (1, 0): [5, 'You are in the void of space, the sheer amount of nothingness is eerie.'],
-            (0, 1): [5, 'You are in the void of space, the sheer amount of nothingness is eerie.'],
-            (1, 1): [5, 'You are in the void of space, the sheer amount of nothingness is eerie.']
+            (0, 0): [5, '', "\033[37m\033[40m - \033[m"],
+            (1, 0): [5, '', "\033[37m\033[40m - \033[m"],
+            (0, 1): [5, '', "\033[37m\033[40m - \033[m"],
+            (1, 1): [5, '', "\033[37m\033[40m - \033[m"]
         }
-
-        scan_space_grid(rows, columns, space, character)
-        expected_output = '[X] - \n -  $ '
+        scan_space_grid(character, space, 2, 2)
+        expected_output = ("\033[7m[X]\033[m\033[37m\033[40m - \033[m\n\033[37m\033[40m - \033[m\033[37"
+                           "m\033[40m - \033[m\n")
         self.assertEqual(mock_stdout.getvalue(), expected_output)
