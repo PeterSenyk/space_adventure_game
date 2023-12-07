947f5bddffa883b1fa929723460660966f6ec12a
PeterSenyk <Peter.Senyk@gmail.com>
2023-12-07 14:45:33 -0800

    ADD unit test for make_space

--- unittests/boards/test_make_space.py
+++ unittests/boards/test_make_space.py
@@ -1,12 +1,25 @@
-from unittest import TestCase
-from unittest.mock import patch
-from boards import make_space
+import unittest
+from boards import make_space, space_tiles_dict
 
 
-class Test(TestCase):
-    @patch('__main__.populate_space')
-    def test_make_space_2_2(self, mock_populate_space):
-        mock_populate_space.return_value = [5, 'You are in the void of space, the sheer amount of nothingness is eerie.']
-        space = make_space(2, 2)
-        for tile in space.values():
-            self.assertEqual(tile, [99, "Mocked Tile Description"])
+class TestMakeSpace(unittest.TestCase):
+
+    def test_make_space_5_5(self):
+        rows = 2
+        columns = 2
+        min_event = 0
+        max_event = 0
+        actual = make_space(rows, columns, min_event, max_event)
+        expected = {(0, 0): [0,
+                             "You're in the docking bay of the Arc-Corp training academy.",
+                             'AC1'],
+                    (0, 1): [0,
+                             "You're in the docking bay of the Arc-Corp training academy.",
+                             'AC1'],
+                    (1, 0): [0,
+                             "You're in the docking bay of the Arc-Corp training academy.",
+                             'AC1'],
+                    (1, 1): [0,
+                             "You're in the docking bay of the Arc-Corp training academy.",
+                             'AC1']}
+        self.assertEqual(expected, actual)
