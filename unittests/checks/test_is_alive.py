82b92350c90dabe857a5fd16675fca7737488fb9
PeterSenyk <Peter.Senyk@gmail.com>
2023-12-07 14:06:45 -0800

    FIX unit test is_alive

--- unittests/checks/test_is_alive.py
+++ unittests/checks/test_is_alive.py
@@ -7,8 +7,8 @@ class Test(TestCase):
     def test_is_alive(self):
         character = {
             'Stats': {'Name': 'Captain sen'},
-            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': 5,
-                     'Targeting': 4, 'Shield': 2, 'Cargo': 2},
+            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': [5, 5],
+                     'Targeting': 4, 'Shield': [2, 2], 'Cargo': 2},
             'Coordinates': {'X-coordinate': 4, 'Y-coordinate': 4}
         }
         actual = is_alive(character)
@@ -18,8 +18,8 @@ class Test(TestCase):
     def test_is_not_alive(self):
         character = {
             'Stats': {'Name': 'Captain sen'},
-            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': 0,
-                     'Targeting': 4, 'Shield': 2, 'Cargo': 2},
+            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': [0, 5],
+                     'Targeting': 4, 'Shield': [2, 2], 'Cargo': 2},
             'Coordinates': {'X-coordinate': 4, 'Y-coordinate': 4}
         }
         actual = is_alive(character)
@@ -29,8 +29,8 @@ class Test(TestCase):
     def test_is_very_not_alive(self):
         character = {
             'Stats': {'Name': 'Captain sen'},
-            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': -10,
-                     'Targeting': 4, 'Shield': 2, 'Cargo': 2},
+            'Ship': {'Ship': 'AEGIS GLADIUS', 'Attack': 2, 'Movement': 3, 'HP': [-10, 5],
+                     'Targeting': 4, 'Shield': [2, 2], 'Cargo': 2},
             'Coordinates': {'X-coordinate': 4, 'Y-coordinate': 4}
         }
         actual = is_alive(character)
