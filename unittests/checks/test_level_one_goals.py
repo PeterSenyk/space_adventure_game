5f1a47da3c3ece61556b95905bf75c9ffbea6584
PeterSenyk <Peter.Senyk@gmail.com>
2023-12-07 14:33:11 -0800

    ADD unit tests for level_two_goal

--- unittests/checks/test_level_one_goals.py
+++ unittests/checks/test_level_one_goals.py
@@ -1,13 +1,16 @@
 from unittest import TestCase
 from checks import level_one_goal
 
+
 class Test(TestCase):
+
     def test_level_one_goal_not_complete(self):
         player = {
             "Stats": {"Title": "Trainee", "Name": "no goal", "Accolades": {"Ships Destroyed": 0, "Debris Avoided": 0}},
             "Ship": {"Name": "test ship", "Attack": 2, "Movement": 2, "HP": [5, 5], "Targeting": 4,
-             "Shield": [2, 2], "Cargo": []},
+                     "Shield": [2, 2], "Cargo": []},
             "Coordinates": {"X-coordinate": 0, "Y-coordinate": 0}}
+
         actual = level_one_goal(player)
         expected = False
         self.assertEqual(expected, actual)
---
+++ unittests/checks/test_level_two_goal.py
@@ -0,0 +1,47 @@
+from unittest import TestCase
+import events
+from checks import level_two_goal
+
+
+class Test(TestCase):
+
+    def test_level_one_goal_not_complete(self):
+        player = {
+            "Stats": {"Title": "Trainee", "Name": "miss goal", "Accolades": {"Ships Destroyed": 0, "Debris Avoided": 0}},
+            "Ship": {"Name": "test ship", "Attack": 2, "Movement": 2, "HP": [5, 5], "Targeting": 4,
+                     "Shield": [2, 2], "Cargo": []},
+            "Coordinates": {"X-coordinate": 0, "Y-coordinate": 0}}
+        events.bring_back_stolen_tech(player)
+        actual = level_two_goal(player)
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_level_one_goal_complete(self):
+        player = {
+            "Stats": {"Title": "Trainee", "Name": "miss goal", "Accolades": {"Ships Destroyed": 0, "Debris Avoided": 0}},
+            "Ship": {"Name": "test ship", "Attack": 2, "Movement": 2, "HP": [5, 5], "Targeting": 4,
+                     "Shield": [2, 2], "Cargo": ["Explorer Class Quantum Drive"]},
+            "Coordinates": {"X-coordinate": 2, "Y-coordinate": 6}}
+        actual = level_two_goal(player)
+        expected = True
+        self.assertEqual(expected, actual)
+
+    def test_level_one_goal_not_complete_coordinates(self):
+        player = {
+            "Stats": {"Title": "Trainee", "Name": "miss goal", "Accolades": {"Ships Destroyed": 0, "Debris Avoided": 0}},
+            "Ship": {"Name": "test ship", "Attack": 2, "Movement": 2, "HP": [5, 5], "Targeting": 4,
+                     "Shield": [2, 2], "Cargo": ["Explorer Class Quantum Drive"]},
+            "Coordinates": {"X-coordinate": 2, "Y-coordinate": 7}}
+        actual = level_two_goal(player)
+        expected = False
+        self.assertEqual(expected, actual)
+
+    def test_level_one_goal_not_complete_cargo(self):
+        player = {
+            "Stats": {"Title": "Trainee", "Name": "miss goal", "Accolades": {"Ships Destroyed": 0, "Debris Avoided": 0}},
+            "Ship": {"Name": "test ship", "Attack": 2, "Movement": 2, "HP": [5, 5], "Targeting": 4,
+                     "Shield": [2, 2], "Cargo": []},
+            "Coordinates": {"X-coordinate": 2, "Y-coordinate": 6}}
+        actual = level_two_goal(player)
+        expected = False
+        self.assertEqual(expected, actual)
